0330ba709d5217c773622da442e9c27b3befeae0
James Cotton <peabody124@gmail.com>
2017-07-26 15:22:18 -0500

    Quadcopter: do not inherit ForwardWalker and add test platform
    Test platform will eventually be zoo agent.

---
+++ agent_zoo/RoboschoolQuadcopterHover_v0_2017jul.py
@@ -0,0 +1,44 @@
+import os.path, time, gym
+import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+import roboschool
+
+
+def demo_run():
+    config = tf.ConfigProto(
+        inter_op_parallelism_threads=1,
+        intra_op_parallelism_threads=1,
+        device_count = { "GPU": 0 } )
+    sess = tf.InteractiveSession(config=config)
+
+    env = gym.make("RoboschoolQuadcopterHover-v0")
+
+    while 1:
+        frame = 0
+        score = 0
+        restart_delay = 0.0
+        obs = env.reset()
+
+        a = np.zeros(4,)
+
+        while 1:
+
+            obs, r, done, _ = env.step(a)
+
+            score += r
+            frame += 1
+            still_open = env.render("human")
+            if still_open==False:
+                return
+            if not done: continue
+            if restart_delay==0.0:
+                print("score=%0.2f in %i frames" % (score, frame))
+                if still_open!=True:      # not True in multiplayer or non-Roboschool environment
+                    break
+                restart_delay = time.time() + 1.0
+            if time.time() > restart_delay:
+                break
+
+if __name__=="__main__":
+    demo_run()
--- roboschool/gym_quadcopter.py
+++ roboschool/gym_quadcopter.py
@@ -1,15 +1,14 @@
 from roboschool.gym_forward_walker import RoboschoolForwardWalker
 from roboschool.gym_urdf_robot_env import RoboschoolUrdfEnv
+from roboschool.multiplayer import SharedMemoryClientEnv
 from roboschool.scene_abstract import cpp_household
 from roboschool.scene_stadium import SinglePlayerStadiumScene
 import numpy as np
 
-class RoboschoolQuadcopterHover(RoboschoolForwardWalker, RoboschoolUrdfEnv):
+class RoboschoolQuadcopterBase(SharedMemoryClientEnv, RoboschoolUrdfEnv):
     random_yaw = False
-    foot_list = ["FL", "FR", "BL", "BR"]
 
     def __init__(self):
-        RoboschoolForwardWalker.__init__(self, power=0.30)
         RoboschoolUrdfEnv.__init__(self,
             "quadcopter_description/urdf/quadcopter-v1.urdf",
             "center",
@@ -17,21 +16,25 @@ class RoboschoolQuadcopterHover(RoboschoolForwardWalker, RoboschoolUrdfEnv):
             fixed_base=False,
             self_collision=False)
 
+        self.start_pos_x, self.start_pos_y, self.start_pos_z = 0, 0, 0
+        self.camera_x = 0
+        self.camera_y = 4.3
+        self.camera_z = 45.0
+        self.camera_follow = 0
+
     def create_single_player_scene(self):
         return SinglePlayerStadiumScene(gravity=9.8, timestep=0.0165/8, frame_skip=8)   # 8 instead of 4 here
 
-    def alive_bonus(self, z, pitch):
-        x,y,z = self.base.pose().xyz()
-        print("Position: " + self.base.pose().xyz())
+    def apply_action(self, a):
+        assert( np.isfinite(a).all() )
 
-        return 1 # TODO: bonus for at certain position
+        # TODO: implement quadcopter physics here
+        # ? how to use bullet
 
     def robot_specific_reset(self):
-        RoboschoolForwardWalker.robot_specific_reset(self)
+        self.scene.actor_introduce(self)
         self.set_initial_orientation(yaw_center=0, yaw_random_spread=np.pi)
         self.base = self.parts["center"]
-
-    random_yaw = False
 
     def set_initial_orientation(self, yaw_center, yaw_random_spread):
         cpose = cpp_household.Pose()
@@ -44,3 +47,97 @@ class RoboschoolQuadcopterHover(RoboschoolForwardWalker, RoboschoolUrdfEnv):
         cpose.set_rpy(0, 0, yaw)  # just face random direction, but stay straight otherwise
         self.cpp_robot.set_pose_and_speed(cpose, 0,0,0)
         self.initial_z = 1.5
+
+    def calc_state(self):
+    	# state is just concatenation of position and orientation
+    	# can later augment with additional information like velocities
+    	# and rotational moments. can additional corrupt with noise.
+    	state = np.concatenate((self.base.pose().xyz(), self.base.pose().quatertion()))
+    	#print("State: {}".format(state))
+    	return state
+
+    def _step(self, a):
+        if not self.scene.multiplayer:  # if multiplayer, action first applied to all robots, then global step() called, then _step() for all robots with the same actions
+            self.apply_action(a)
+            self.scene.global_step()
+
+        state = self.calc_state()  # also calculates self.joints_at_limit
+
+        alive = float(self.alive_bonus())   # state[0] is body height above ground, body_rpy[1] is pitch
+        done = alive < 0
+        if not np.isfinite(state).all():
+            print("~INF~", state)
+            done = True
+
+        self.rewards = [
+            alive,
+            ]
+
+        self.frame  += 1
+        if (done and not self.done) or self.frame==self.spec.timestep_limit:
+            self.episode_over(self.frame)
+        self.done   += done   # 2 == 1+True
+        self.reward += sum(self.rewards)
+        self.HUD(state, a, done)
+        return state, sum(self.rewards), bool(done), {}
+
+    def episode_over(self, frames):
+        pass
+
+    def camera_adjust(self):
+        #self.camera_dramatic()
+        self.camera_simple_follow()
+
+    def camera_simple_follow(self):
+        x, y, z = self.body_xyz
+        self.camera_x = 0.98*self.camera_x + (1-0.98)*x
+        self.camera.move_and_look_at(self.camera_x, y-2.0, 1.4, x, y, 1.0)
+
+    def camera_dramatic(self):
+        pose = self.robot_body.pose()
+        speed = self.robot_body.speed()
+        x, y, z = pose.xyz()
+        if 1:
+            camx, camy, camz = speed[0], speed[1], 2.2
+        else:
+            camx, camy, camz = self.walk_target_x - x, self.walk_target_y - y, 2.2
+
+        n = np.linalg.norm([camx, camy])
+        if n > 2.0 and self.frame > 50:
+            self.camera_follow = 1
+        if n < 0.5:
+            self.camera_follow = 0
+        if self.camera_follow:
+            camx /= 0.1 + n
+            camx *= 2.2
+            camy /= 0.1 + n
+            camy *= 2.8
+            if self.frame < 1000:
+                camx *= -1
+                camy *= -1
+            camx += x
+            camy += y
+            camz  = 1.8
+        else:
+            camx = x
+            camy = y + 4.3
+            camz = 2.2
+        #print("%05i" % self.frame, self.camera_follow, camy)
+        smoothness = 0.97
+        self.camera_x = smoothness*self.camera_x + (1-smoothness)*camx
+        self.camera_y = smoothness*self.camera_y + (1-smoothness)*camy
+        self.camera_z = smoothness*self.camera_z + (1-smoothness)*camz
+        self.camera.move_and_look_at(self.camera_x, self.camera_y, self.camera_z, x, y, 0.6)
+
+
+class RoboschoolQuadcopterHover(RoboschoolQuadcopterBase):
+
+    def __init__(self):
+        RoboschoolQuadcopterBase.__init__(self)
+    def alive_bonus(self):
+        x,y,z = self.base.pose().xyz()
+
+        if z > 0 and z < 5:
+            return 1
+        else:
+            return 0
